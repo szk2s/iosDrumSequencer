93b1a5e98e4a1ca4127b5670f4307fff480bef29
szk2s <suzukisjss@gmail.com>
2021-09-06 19:37:51 +0900

    Initial Commit

---
+++ iosDrumSequencer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iosDrumSequencer
+//
+//  Created by Satoshi Suzuki on 2021/09/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
